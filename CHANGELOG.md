c0269aa84c0be88fb74c35631a1fed85fcf09d87
Sven <sven@cubyte.nl>
2018-10-21 14:05:12 +0200

    Setup basic cookbook structure

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# chef-kubernetes CHANGELOG
+
+This file is used to list changes made in each version of the chef-kubernetes cookbook.
+
+# 0.1.0
+
+Initial release.
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'chef-kubernetes'
+maintainer 'Sven Mollinga'
+maintainer_email 'sven@cubyte.nl'                                                         
+license 'All Rights Reserved'                                                              
+description 'Installs/Configures kubernetes'                                               
+long_description 'Installs/Configures kubernetes'                                          
+version '0.1.0'                                                                            
+chef_version '>= 13.0'                                                                     
+                                                                                           
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/GeekintheMiddle/chef-kuberneteshttps://github.com/GeekintheMiddle/chef-kubernetes'
+
