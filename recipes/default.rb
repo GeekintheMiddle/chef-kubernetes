caa1454e84de7e57ebfdd4102777c3c9832b78ef
Sven <sven@cubyte.nl>
2018-10-21 15:30:46 +0200

    Adding kubernetes repo and install the package Kubectl

---
+++ recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: chef-kubernetes
+# Recipe:: default
+#
+# Copyright:: 2018, Sven Mollinga, All Rights Reserved.
+
+include_recipe "chef-kubernetes::packages"
---
+++ recipes/packages.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook:: chef-kubernetes
+# Recipe:: packages
+#
+# Copyright:: 2018, Sven Mollinga, All Rights Reserved.
+
+if node[:platform].include?("ubuntu")
+  if node[:platform_version].include?("18.04")
+    apt_repository 'kubernetes' do
+      uri        'http://apt.kubernetes.io/'
+      components ['main']
+      distribution 'kubernetes-xenial'
+      key ['BA07F4FB', 'https://packages.cloud.google.com/apt/doc/apt-key.gpg']
+    end
+  end
+end
+
+package "kubectl" do
+  action :install
+end
